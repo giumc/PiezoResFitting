a25f1ea3702edd430e7bf842ebc208b18483bfc8
giumc <54653802+giumc@users.noreply.github.com>
2020-02-17 21:18:35 -0500

    Update resonator.m

--- @resonator/resonator.m
+++ @resonator/resonator.m
@@ -3,10 +3,27 @@ classdef resonator < handle
         anus=0;
     end
     
-    methods (Static)
-        function getanus()
+    methods %Constructor
+        function obj=resonator()
+            setanus(obj);
+        end
+    end
+            
+            
+    methods
+        function getanus(obj)
             fprintf("This is an anus!\n");
-            fprintf("Currently there are  %d anuses",anus);
+            fprintf("Currently there are  %d anuses\n",obj.anus);
+            
+        end
+        function setanus(obj)
+            fprintf('How many anuses in a resonator?\n');
+            prompt = {'How many anuses for a resonator?'};
+            dlgtitle = 'Question';
+            dims = 1;
+            definput = {'3'};
+            answer = inputdlg(prompt,dlgtitle,dims,definput);
+            obj.anus=str2num(answer{1});
         end
     end
 end
