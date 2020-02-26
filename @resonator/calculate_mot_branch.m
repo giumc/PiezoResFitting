0ec965d35cef719eba8b615cab1156809de84a1b
giumc <54653802+giumc@users.noreply.github.com>
2020-02-26 08:50:50 -0500

    Update calculate_mot_branch.m

--- @resonator/calculate_mot_branch.m
+++ @resonator/calculate_mot_branch.m
@@ -1,12 +1,12 @@
 function mot_branch=calculate_mot_branch(resonator,index)
     fres    =   resonator.mode(index).fres;
     q       =   resonator.mode(index).q;
-    kt2     =   resonator.mode(index). kt2;
+    kt2     =   resonator.mode(index).kt2;
     c0      =   resonator.c0;
-    
+
     mot_branch.Lm   =   pi^2 / 8  / (2*pi*fres)^2 / c0 / kt2 ;
-    
+
     mot_branch.Cm   =   8 / pi^2 * c0 * kt2 ;
-    
+
     mot_branch.Rm   =   pi^2 /8 / (2*pi*fres) / c0 / kt2 /q ;
 end
