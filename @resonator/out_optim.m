ce07c6079ca516cf397ae30b81f07678bddbc06e
giumc <54653802+giumc@users.noreply.github.com>
2020-02-26 08:45:56 -0500

    table_update

--- @resonator/out_optim.m
+++ @resonator/out_optim.m
@@ -3,6 +3,7 @@ function stop   =     out_optim (res, x, flag,state)
     if strcmp(state,'iter')
         res.array_to_variables(x);
         res.plot_data;
+        res.table_res;
         pause(1)
     end
     
--- @resonator/table_res.m
+++ @resonator/table_res.m
@@ -8,24 +8,28 @@ function table_res(res)
         res.values_table=tab;
     end
         tab.Units='normalized';
+        Row1{1}=sprintf('Fres [Hz]');
+        Row1{2}=sprintf('kt2');
+        Row1{3}=sprintf('Q');
         for k=1:length(res.mode)
-            Row1{1,k}=sprintf('mode');
-            Row1{2,k}=sprintf('Fres [Hz]');
-            Row1{3,k}=sprintf('kt2');
-            Row1{4,k}=sprintf('Q');
-            Data1{1,k}=k;
-            Data1{2,k}=res.mode(k).fres;
-            Data1{3,k}=res.mode(k).kt2;
-            Data1{4,k}=res.mode(k).q;
+            
+            Data1(1,k)=res.mode(k).fres;
+            Data1(2,k)=res.mode(k).kt2;
+            Data1(3,k)=res.mode(k).q;
+            Data1(4:6,k)=0;
         end
-         Row2={'c0 [F]';'R0 [\omega]';'Rs [\oega]'};
+         Row2={'c0 [F]';'R0 [Ohm]';'Rs [Ohm]'};
         previousFormat=get(0,'format');
         format shortG
-        Data2={...
-            res.c0;res.r0;res.rs};
-         tab.RowName={Row1{:},Row2{:}};
-         tab.Data=[Data1{:},Data2{:}].';
-         tab.ColumnName='Value';
+        Data2=[0;0;0;
+            res.c0;res.r0;res.rs];
+         tab.RowName=[Row1(:)',Row2(:)'];
+         tab.Data=[Data2,Data1];
+         column_names{1}='Value';
+         for i=1:length(res.mode)
+             column_names{i+1}=sprintf('Mode %d',i);
+         end
+         tab.ColumnName=column_names;
          format(previousFormat)   
         tab.Position(3:4)=tab.Extent(3:4);
         tab.Position(1:2)=[0.6 0.5];
